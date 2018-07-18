5b454b82bd6dfef1eec4515653c7c1676da9abc0
Thomas <thomas.gitcommit@outlook.com>
2018-07-18 22:32:49 +1000

    Formatting changes and fixed accessibility

--- TGLibrary/Maths.cs
+++ TGLibrary/Maths.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Linq;
 
-namespace TGLibrary
-{
-	class Maths
-	{	
-		static double standDev(double[] dNumbers, int round = 0)
-		{	double[] tempNumbers = new double[dNumbers.Length];
-			double mean = dNumbers.Average();
-			
-			for(int x = 0; x < dNumbers.Length; x++)
-			{	tempNumbers[x] = Math.Pow( (dNumbers[x] - mean ), 2D);
-			}
-			
-			if (round > 0)
-			{	return Math.Round( Math.Sqrt( tempNumbers.Average() ), round, MidpointRounding.AwayFromZero);
-			} else
-			{	return Math.Sqrt( tempNumbers.Average() );
-			}
-		}
-		
-		static uint SumUpTo(uint n)
-		{	return ( n * ( n + 1 ) ) / 2;
-		}
-	}
+namespace TGLibrary {
+    public static class Maths {
+        public static double StandDev(double[] dNumbers, int round = 0) {
+            double[] tempNumbers = new double[dNumbers.Length];
+            double mean = dNumbers.Average();
+
+            for (int x = 0; x < dNumbers.Length; x++) {
+                tempNumbers[x] = Math.Pow((dNumbers[x] - mean), 2D);
+            }
+
+            if (round > 0) {
+                return Math.Round(Math.Sqrt(tempNumbers.Average()), round, MidpointRounding.AwayFromZero);
+            }
+            else {
+                return Math.Sqrt(tempNumbers.Average());
+            }
+        }
+
+        public static uint SumUpTo(uint n) {
+            return (n * (n + 1)) / 2;
+        }
+    }
 }
