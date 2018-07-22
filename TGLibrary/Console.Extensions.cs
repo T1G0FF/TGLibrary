860bfd98b91513a1017d11e31f9b7f14870f4436
Thomas <thomas.gitcommit@outlook.com>
2018-07-22 14:16:30 +1000

    Add ReplaceLine function.

--- TGLibrary/Console.Extensions.cs
+++ TGLibrary/Console.Extensions.cs
@@ -12,6 +12,23 @@ namespace TGLibrary {
                 } while (!Directory.Exists(folderPath));
                 return folderPath;
             }
+
+            public static void ReplaceLine(string text, Text.Align alignment = Text.Align.Left) {
+                int width = Console.WindowWidth - 1;
+
+                switch (alignment) {
+                    default:
+                    case Text.Align.Left:
+                        Console.Write($"\r{text.PadRight(width)}");
+                        break;
+                    case Text.Align.Right:
+                        Console.Write($"\r{text.PadLeft(width)}");
+                        break;
+                    case Text.Align.Center:
+                        Console.Write($"\r{Text.Center(width, text)}");
+                        break;
+                }       
+            }
         }
     }
 }
