8452e44d71fde7159fa6d6e857ced1bfac0d0407
Thomas <thomas.gitcommit@outlook.com>
2018-07-22 14:10:20 +1000

    Remove redundant variables.

--- TGLibrary/Config.cs
+++ TGLibrary/Config.cs
@@ -5,9 +5,7 @@ namespace TGLibrary {
     /// Contains settings default configuration.
     /// </remarks>
     public static class Config {
-        private static readonly int _DEFAULTWIDTH = Console.WindowWidth;
-        private static readonly int _DEFAULTROTATION = 13;
-        public static int DefaultWidth => _DEFAULTWIDTH;
-        public static int DefaultRotation => _DEFAULTROTATION;
+        public static int DefaultWidth { get; } = Console.WindowWidth;
+        public static int DefaultRotation { get; } = 13;
     }
 }
