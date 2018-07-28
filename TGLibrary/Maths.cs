09f80e5244cd41e989b090a960abb71ff4f0299f
Thomas <thomas.gitcommit@outlook.com>
2018-07-28 13:06:29 +1000

    StandDev now accepts rounding parameters. Added IsCloseCompare().

--- TGLibrary/Maths.cs
+++ TGLibrary/Maths.cs
@@ -2,8 +2,9 @@
 using System.Linq;
 
 namespace TGLibrary {
-    public static class Maths {
-        public static double StandDev(double[] dNumbers, int round = 0) {
+    public static class MathTG {
+        public static double StandDev(double[] dNumbers, int round = -1, MidpointRounding rounding = MidpointRounding.AwayFromZero) {
+            double result = 0;
             double[] tempNumbers = new double[dNumbers.Length];
             double mean = dNumbers.Average();
 
@@ -11,16 +12,43 @@ namespace TGLibrary {
                 tempNumbers[x] = Math.Pow((dNumbers[x] - mean), 2D);
             }
 
-            if (round > 0) {
-                return Math.Round(Math.Sqrt(tempNumbers.Average()), round, MidpointRounding.AwayFromZero);
+            result = Math.Sqrt(tempNumbers.Average());
+            if (round > -1) {
+                result = Math.Round(result, round, rounding);
             }
-            else {
-                return Math.Sqrt(tempNumbers.Average());
-            }
+
+            return result;
         }
 
         public static uint SumUpTo(uint n) {
             return (n * (n + 1)) / 2;
+        }
+
+        public static bool IsCloseCompare(double a, double b, double threshold) {
+            return a >= b - threshold && a <= b + threshold;
+        }
+
+        public static int CloseCompare(double a, double b, double threshold) {
+            int result = 0;
+            if (a >= b - threshold && a <= b + threshold) {
+                result = 0;
+            }
+            else if (a > b + threshold) {
+                result = 1;
+            }
+            else if (a < b - threshold) {
+                result = -1;
+            }
+            return result;
+        }
+
+        public static T Clamp<T>(this T value, T min, T max) where T : IComparable<T> {
+            if (value.CompareTo(min) < 0)
+                return min;
+            else if (value.CompareTo(max) > 0)
+                return max;
+            else
+                return value;
         }
     }
 }
