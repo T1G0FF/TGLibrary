211ea43ef71b30ae1ec5050ef7a9eda71d7c63f2
Thomas <Thomas.GitCommit@outlook.com>
2021-02-12 09:03:02 +1100

    Update Console.Alert.cs
    Added title parameters to most TGConsole.Alerts.
    Shifted width to an optional parameter.

--- TGConsole/Console.Alert.cs
+++ TGConsole/Console.Alert.cs
@@ -4,71 +4,71 @@ using System.Text;
 namespace TGConsole {
 	public class Alert {
 		#region Public Methods
-		public static string Title(string title) {
-			return Title(Config.ConsoleWidth, title);
-		}
-		public static string Title(int width, string title) {
+		public static string Title(string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
 			string topLine = Draw.Line(width, '╔', '═', '╗') + Environment.NewLine;
-			string midLine = Text.FitCenter(width, title, '║', ' ');
+			string midLine = Text.FitCenter(width, text, '║', ' ');
 			string btmLine = Draw.Line(width, '╚', '═', '╝');
-			return AlertBuilder(width, topLine, midLine, btmLine);
+			return AlertBuilder(topLine, midLine, btmLine);
 		}
 
-		public static string Message(string msg) {
-			return Message(Config.ConsoleWidth, msg);
-		}
-		public static string Message(int width, string msg) {
+		public static string Message(string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
 			string topLine = Draw.Line(width, '┌', '─', '┐') + Environment.NewLine;
-			string midLine = Text.FitCenter(width, msg, '│');
+			string midLine = Text.FitCenter(width, text, '│');
 			string btmLine = Draw.Line(width, '└', '─', '┘');
-			return AlertBuilder(width, topLine, midLine, btmLine);
+			return AlertBuilder(topLine, midLine, btmLine);
+		}
+		public static string Message( string title, string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			string topLine = Text.FitCenter(width, title, '┌', '─', '┐');
+			string midLine = Text.FitCenter(width, text, '│');
+			string btmLine = Draw.Line(width, '└', '─', '┘');
+			return AlertBuilder(topLine, midLine, btmLine);
 		}
 
-		public static string Info(string msg) {
-			return Info(Config.ConsoleWidth, msg);
+		public static string Info(string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			return Info("Information", text, width);
 		}
-		public static string Info(int width, string msg) {
-			string topLine = Text.FitLeft(width, "Information", '┌', '─', '┐');
-			string midLine = Text.FitLeft(width, msg, '│');
+		public static string Info(string title,  string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			string topLine = Text.FitLeft(width, title, '┌', '─', '┐');
+			string midLine = Text.FitLeft(width, text, '│');
 			string btmLine = Draw.Line(width, '└', '─', '┘');
-			return AlertBuilder(width, topLine, midLine, btmLine);
+			return AlertBuilder(topLine, midLine, btmLine);
 		}
 
-		public static string Usage(string usage) {
-			return Usage(Config.ConsoleWidth, usage);
+		public static string Usage(string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			return Usage("? Usage ?", text, width);
 		}
-		public static string Usage(int width, string usage) {
-			string topLine = Text.FitCenter(width, "? Usage ?", '?', '─');
-			string midLine = Text.FitLeft(width, usage, '│');
+		public static string Usage(string title, string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			string topLine = Text.FitCenter(width, title, '?', '─');
+			string midLine = Text.FitLeft(width, text, '│');
 			string btmLine = Draw.Line(width, '?', '─');
-			return AlertBuilder(width, topLine, midLine, btmLine);
+			return AlertBuilder(topLine, midLine, btmLine);
 		}
 
-		public static string Error(string error) {
-			return Error(Config.ConsoleWidth, error);
+		public static string Error(string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			return Error("! Error !", text, width);
 		}
-		public static string Error(int width, string error) {
-			string topLine = Text.FitCenter(width, "! Error !", '!', '-');
-			string midLine = Text.FitCenter(width, error, '|');
+		public static string Error(string title, string text, int width = -1) {
+			width = (width > 0) ? width : Config.ConsoleWidth;
+			string topLine = Text.FitCenter(width, title, '!', '-');
+			string midLine = Text.FitCenter(width, text, '|');
 			string btmLine = Draw.Line(width, '!', '-');
-			return AlertBuilder(width, topLine, midLine, btmLine);
+			return AlertBuilder(topLine, midLine, btmLine);
 		}
-		#endregion
 
-		#region Private Methods
-		private static string AlertBuilder(int width, string topLine, string midLine, string btmLine) {
+		public static string AlertBuilder(string topLine, string midLine, string btmLine) {
 			StringBuilder sb = new StringBuilder();
 
-			if (false && (!ConsoleFunctions.ConsolePresent || (ConsoleFunctions.ConsolePresent && width < Console.WindowWidth))) {
-				sb.AppendLine(topLine);
-				sb.AppendLine(midLine);
-				sb.Append(btmLine);
-			}
-			else {
-				sb.Append(topLine);
-				sb.Append(midLine);
-				sb.Append(btmLine);
-			}
+			sb.Append(topLine);
+			sb.Append(midLine);
+			sb.Append(btmLine);
 
 			return sb.ToString();
 		}
