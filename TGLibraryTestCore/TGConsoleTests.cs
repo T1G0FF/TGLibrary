8bb6c436d45b6137c06b9e135331634e4351b460
Thomas <Thomas.GitCommit@outlook.com>
2021-03-03 20:07:26 +1100

    Update TGConsoleTests.cs

--- TGLibraryTestCore/TGConsoleTests.cs
+++ TGLibraryTestCore/TGConsoleTests.cs
@@ -5,13 +5,20 @@ using System.Text;
 using TGConsole;
 
 namespace TGLibraryTestCore {
-    [TestClass]
-    public class TGConsoleTests {
-        [TestMethod]
-        public void TGConsoleTests_1() {
-            var titleStr = (new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" }).ToString("\n");
-			var alertStr = Alert.Info("Converted Images", titleStr);
+	[TestClass]
+	public class TGConsoleTests {
+		[TestMethod]
+		public void TGConsoleTests_1CharNewLine() {
+			var titleStr = (new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" }).ToString("\n");
+			var alertStr = Alert.Info("Title Here", titleStr);
 			Assert.AreEqual(7, alertStr.Split("\n").Length);
-        }
-    }
+		}
+
+		[TestMethod]
+		public void TGConsoleTests_2CharNewline() {
+			var titleStr = (new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" }).ToString("\r\n");
+			var alertStr = Alert.Info("Title Here", titleStr);
+			Assert.AreEqual(7, alertStr.Split("\n").Length);
+		}
+	}
 }
