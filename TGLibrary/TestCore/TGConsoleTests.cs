d54f9b5032d895f1c65b3f39ad72a8ab8fcb0e3b
Thomas <thomas.gitcommit@outlook.com>
2023-12-20 12:37:25 +1100

    Update TGConsoleTests.cs
    Updated for consistency across tests.
    Added tests for width when using FitToConsole & FitToText.

--- TGLibrary/TestCore/TGConsoleTests.cs
+++ TGLibrary/TestCore/TGConsoleTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -9,16 +10,39 @@ namespace TGLibraryTestCore {
 	public class TGConsoleTests {
 		[TestMethod]
 		public void TGConsoleTests_1CharNewLine() {
-			var titleStr = (new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" }).ToString("\n");
-			var alertStr = Alert.Info("Title Here", titleStr);
-			Assert.AreEqual(7, alertStr.Split("\n").Length);
+			var titleStr = "Title Here";
+			var contentArray = new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" };
+			var contentStr = contentArray.ToString("\n");
+			var alertStr = Alert.Info(titleStr, contentStr);
+			Assert.AreEqual(contentArray.Length + 2, alertStr.Split("\n").Length); // Line count plus top and bottom row.
 		}
 
 		[TestMethod]
 		public void TGConsoleTests_2CharNewline() {
-			var titleStr = (new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" }).ToString("\r\n");
-			var alertStr = Alert.Info("Title Here", titleStr);
-			Assert.AreEqual(7, alertStr.Split("\n").Length);
+			var titleStr = "Title Here";
+			var contentArray = new string[] { "Line 1", "Line 2", "Line 3", "Line 4", "Line 5" };
+			var contentStr = contentArray.ToString("\r\n");
+			var alertStr = Alert.Info(titleStr, contentStr);
+			Assert.AreEqual(contentArray.Length + 2, alertStr.Split("\n").Length); // Line count plus top and bottom row.
+		}
+
+		[TestMethod]
+		public void TGConsoleTests_FitToConsole() {
+			var titleStr = "Title Here";
+			var contentStr = "Content Goes Here";
+			var alertStr = Alert.Info(titleStr, contentStr, Alert.FitTo.Console);
+			var topLine = alertStr.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None)[0];
+			Assert.AreEqual(Config.ConsoleWidth, topLine.Length);
+		}
+
+		
+		[TestMethod]
+		public void TGConsoleTests_FitToText() {
+			var titleStr = "Title Here";
+			var contentStr = "Content Goes Here";
+			var alertStr = Alert.Info(titleStr, contentStr, Alert.FitTo.Text);
+			var topLine = alertStr.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None)[0];
+			Assert.AreEqual(contentStr.Length + 4, topLine.Length); // Line length plus 2 Ends and 2 Spaces
 		}
 	}
 }
